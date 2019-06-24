e660e6244071c4fdb187fd9d731dc481c56c910e
lelange <48963241+lelange@users.noreply.github.com>
2019-06-24 14:09:33 +0200

    change comment

--- run_tool.sh
+++ run_tool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Start up a MongoDB daemon instance
-#mkdir ./mongodb
+mkdir ./mongodb
 /usr/bin/mongod --logpath ./mongodb/log.txt --dbpath ./mongodb # have a local folder to drop everything
 python gener_experiment.py --experiment experiments/lange --optimize error --epochs 2 --constraint power --constraint_val 1000.0
